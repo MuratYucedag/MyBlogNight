1713ea0951317b7703f72398c9254e5feb4db527
MuratYucedag <yucedagmurat23@gmail.com>
2024-11-07 20:47:02 +0300

    ...

--- MyBlogNight.PresentationLayer/Controllers/LoginController.cs
+++ MyBlogNight.PresentationLayer/Controllers/LoginController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MyBlogNight.EntityLayer.Concrete;
+using MyBlogNight.PresentationLayer.Models;
 
 namespace MyBlogNight.PresentationLayer.Controllers
 {
@@ -15,5 +16,26 @@ namespace MyBlogNight.PresentationLayer.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginViewModel model)
+        {
+            var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, false, true);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                return View();
+            }
+        }
     }
 }
+/*
+//50 Derste Mvc
+//50 Derste Tatil Seyahat Sitesi
+//Mvc5 Admin Panelli Cv Sitesi
+//Mvc Proje Kampı
+//AspNet Core ile Adım Adım Web Geliştirme
+*/
